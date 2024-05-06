4e4ebfc135f6370c5b1e33f95b4a1f91234ff38a
Md. Shamim Rahman <apubdtea@gmail.com>
2024-05-06 14:02:16 +0600

    feat: define Topic class in models

--- journal_records/models.py
+++ journal_records/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """A topic the user is journaling about."""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __string__(self):
+        """Define a string representation of the string."""
+        return self.text
