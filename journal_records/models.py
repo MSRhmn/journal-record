e49685fe00e91ec5831134efab03cb92b53ddbf4
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-10 13:31:47 +0600

    fix: topic model return function

--- journal_records/models.py
+++ journal_records/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __string__(self):
+    def __str__(self):
         """Return a string representation of the model."""
         return self.text
 
