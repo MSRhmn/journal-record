50e2b5bd19f863aeb3cb0e25979d1c7ccf65c7b4
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-12 17:34:25 +0600

    chore: properly indent entry models str class

--- journal_records/models.py
+++ journal_records/models.py
@@ -24,7 +24,6 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a simple string representing the entry."""
-
         if len(self.text) > 50:
             return f"{self.text[:50]}..."
         else:
