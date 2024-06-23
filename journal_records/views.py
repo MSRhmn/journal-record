fcabb25d323a8dd82b245b7281fb82eeec4963f3
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-23 16:56:19 +0600

    feat: define topics function in views

--- journal_records/views.py
+++ journal_records/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """The homepage for journal records"""
     return render(request, "journal_records/index.html")
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "journal_records/topics.html", context)
