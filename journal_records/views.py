4999f691cce142cac4b87f98d9284e331a8ae7f2
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-26 17:08:48 +0600

    fix: bug in the topic function

--- journal_records/views.py
+++ journal_records/views.py
@@ -17,7 +17,7 @@ def topics(request):
 
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id="topic_id")
+    topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "journal_records/topic.html", context)
