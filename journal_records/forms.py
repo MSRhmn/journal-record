48415177ef313dd6edd86f8442ca8bbcfcb4ff81
Md. Shamim Rahman <apubdtea@gmail.com>
2024-07-03 18:37:27 +0600

    feat: define topic form model in the forms

---
+++ journal_records/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Topic
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        field = ["text"]
+        labels = {"text": ""}
