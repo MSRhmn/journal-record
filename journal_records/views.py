edff7f8d516d17f5488406f2ddfd6ca39b0f7df3
Md. Shamim Rahman <apubdtea@gmail.com>
2024-07-06 12:24:58 +0600

    feat: define new topic adding view function

--- journal_records/views.py
+++ journal_records/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -21,3 +22,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "journal_records/topic.html", context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != "POST":
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process the data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("journal_records:topics")
+
+    # Display a blank or invalid form.
+    context = {"form": form}
+    return render(request, "journal_records/new_topic.html" context)
