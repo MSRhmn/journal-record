1cc6db4384f15fd105c4e34a566847923db48dc2
Md. Shamim Rahman <apubdtea@gmail.com>
2024-11-04 17:47:12 +0600

    fix: new topic view

--- journal_records/views.py
+++ journal_records/views.py
@@ -42,7 +42,9 @@ def new_topic(request):
         # POST data submitted; process the data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("journal_records:topics")
 
     # Display a blank or invalid form.
